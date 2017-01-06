5a1242dffba36d7c4b0d52d909c5c6f6e5a11869
miguedrive <miguee.casas@gmail.com>
2017-01-06 15:16:09 -0300

    Same, but in bash

---
+++ backup4all.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Fancy text
+echo "Backing up databases..."
+
+# Create 'bkp' directory 
+mkdir /root/bkp
+
+# Access into bkp
+cd /root/bkp
+
+# MySQL dump, backup all DB's
+mysqldump --all-databases > everydb.sql
+
+# Now, we should call (or just execute) rotate.sh, the code looks like this:
+
+#### Rotate script for files
+#### v1.2 (4-1-17)
+####
+#Assign variables for Current file (the one that is written), and the "new" one (the 'old' one with name YmdHM)
+
+CUR="/root/bkp/bkpsql" 
+NEW="${CUR}.$(date +%Y-%m-%d---%H-%M)"
+
+
+#### Rename the log
+mv ${CUR} ${NEW}
+touch ${CUR}
+rm /root/bkp/bkpsql
+#### Wait 1 sec before writing the new file
+sleep 1
+#### Compress the old files
+#### xz("-9 ${NEW}")
+
+#Rsync to bkp server
+rsync /root/bkp/everydb.sql root@192.168.1.4:/root/bkp
+
+
